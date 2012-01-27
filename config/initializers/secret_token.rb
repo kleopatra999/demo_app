d87bfb5adcb3ddfa51a18826bfbe53a6512a94f3
Fergus Duncan <fergus@ziplockk.com>
2012-01-27 00:24:57 +0000

    Initial Version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoApp::Application.config.secret_token = '8eb8129185cc86f9d0689ac92e2fe63e5aa313937131a714c99a9f1c5c9616ade32ee0bcf96382d645493582ed1dd509ed0011f22f6c7a10498d3a9c4769c056'
